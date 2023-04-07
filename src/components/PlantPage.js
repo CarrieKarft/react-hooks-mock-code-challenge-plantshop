5f1290a816ad6f6f168f0fe9928990858f50e50a
Carrie Kraft <carriekarft@gmail.com>
2023-04-07 14:47:42 -0700

    commit tree 9816da956f11cab96aac78e39ca1a86635cf3b15

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [plantFilter, setPlantFilter] = useState('')
+
+  console.log(plantFilter)
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(r => r.json())
+    .then(data => setPlants(data))
+  }, [])
+
+  // const MappingPlants = plants.map(plant => plant.name)
+  // console.log(MappingPlants)
+  console.log(plants)
+  const filteringPlants = plants.filter(plant => {
+    if (plant.name.includes(plantFilter.toUpperCase())) {
+      return plant;
+    }
+    return null;
+  })
+  // console.log
+
+  
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} plants={plants}/>
+      <Search plantFilter={plantFilter} setPlantFilter={setPlantFilter}/>
+      <PlantList plants={filteringPlants}/>
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-function Search() {
+function Search({ plantFilter, setPlantFilter }) {
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
         type="text"
         id="search"
+        value={plantFilter}
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={(e) => {
+          // console.log(e.target.value)
+          setPlantFilter(e.target.value)
+        }}
       />
     </div>
   );
